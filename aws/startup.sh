3688fd84d0478a7f126b4eafc56f14bd8013eafc
Dev Chakraborty <devchakraborty@users.noreply.github.com>
2019-06-30 17:34:29 -0700

    Startup script updates

--- aws/startup.sh
+++ aws/startup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 sudo yum update -y
-sudo yum install -y docker git tmux
+sudo amazon-linux-extras install docker
+sudo yum install -y git tmux
 git clone https://github.com/devchakraborty/transient-minecraft.git
 cd transient-minecraft
+sudo service docker start
+sudo gpasswd -a $USER docker
+newgrp docker
 docker build -t minecraft .
 tmux new -s minecraft 'docker run -i minecraft'
