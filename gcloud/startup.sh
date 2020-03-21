6083a126f9320dfab9cdfc2120a2d83b7a9fc8d6
Dev Chakraborty <devchakraborty@users.noreply.github.com>
2020-03-20 20:26:57 -0700

    Update startup script for Debian

--- gcloud/startup.sh
+++ gcloud/startup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 sudo apt update -y
-sudo apt install -y docker git tmux
+sudo apt upgrade -y
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common gnupg2
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt update -y
+sudo apt install -y docker-ce git tmux
 git clone https://github.com/devchakraborty/transient-minecraft.git
 cd transient-minecraft
-sudo service docker start
-sudo gpasswd -a $USER docker
-newgrp docker
 docker build -t minecraft .
 docker run -i minecraft
