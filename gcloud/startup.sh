1b94d857c47a071e98ffcaa29fac794bf5e8429a
Dev Chakraborty <devchakraborty@users.noreply.github.com>
2020-03-23 00:02:32 -0700

    Update startup script

--- gcloud/startup.sh
+++ gcloud/startup.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+# Get deps
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common gnupg2
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update -y
-sudo apt install -y git python3.7 tmux
+sudo apt install -y build-essential libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev zlib1g zlib1g-dev git tmux
+
+# Get, build, and install Python 3.7
+wget https://www.python.org/ftp/python/3.7.7/Python-3.7.7.tgz
+tar -xzvf Python-3.7.7.tgz
+pushd Python-3.7.7
+./configure
+make
+sudo make altinstall
+popd
+
+# Get Minecraft Python code
 git clone https://github.com/devchakraborty/transient-minecraft.git
 cd transient-minecraft
-python3.7 -m pip install poetry
+
+# Get Python deps
+python3.7 -m pip install poetry --user
 python3.7 -m poetry install
+
+# Run server in tmux
 tmux new-session "python3.7 -m poetry run server --cloud gcloud"
