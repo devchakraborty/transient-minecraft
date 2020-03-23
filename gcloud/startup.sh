ec7d3a7b6f5933e46e93d06b4e56c571592640d1
Dev Chakraborty <devchakraborty@users.noreply.github.com>
2020-03-23 00:32:44 -0700

    Update startup script

--- gcloud/startup.sh
+++ gcloud/startup.sh
@@ -3,16 +3,11 @@
 # Get deps
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y build-essential libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev zlib1g zlib1g-dev git tmux
+sudo apt install -y default-jdk build-essential libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev zlib1g zlib1g-dev git reptyr
 
 # Get, build, and install Python 3.7
-wget https://www.python.org/ftp/python/3.7.7/Python-3.7.7.tgz
-tar -xzvf Python-3.7.7.tgz
-pushd Python-3.7.7
-./configure
-make
-sudo make altinstall
-popd
+curl https://pyenv.run | bash
+~/.pyenv/bin/pyenv install 3.7.7
 
 # Get Minecraft Python code
 git clone https://github.com/devchakraborty/transient-minecraft.git
@@ -23,4 +18,5 @@ python3.7 -m pip install poetry --user
 python3.7 -m poetry install
 
 # Run server in tmux
-tmux new-session "python3.7 -m poetry run server --cloud gcloud"
+python3.7 -m poetry run server --cloud gcloud &
+disown %1
