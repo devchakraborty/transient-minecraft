3ebff2a3495105361687f199d2de9cd65c271cdb
Dev Chakraborty <devchakraborty@users.noreply.github.com>
2020-04-07 01:19:24 -0700

    Copy optional env vars

--- transient_minecraft/minecraft/cloud.py
+++ transient_minecraft/minecraft/cloud.py
@@ -22,6 +22,8 @@ import googleapiclient.discovery
 from google.cloud import storage
 from dotenv import load_dotenv
 
+OPTIONAL_ENV_VARS = ["MINECRAFT_VERSION", "MINECRAFT_TERMINATE"]
+
 
 class Cloud(ABC):
     """
@@ -84,8 +86,12 @@ class Cloud(ABC):
         """
         # Generate a line of bash code that writes env vars to a .env file
         env_var_assignments = "".join(
-            f'{env_var}="{os.environ[env_var]}"\n' for env_var in self.required_env_vars
+            f'{env_var}="{os.environ[env_var]}"\n'
+            for env_var in self.required_env_vars
         )
+        for env_var in OPTIONAL_ENV_VARS:
+            if env_var in os.environ:
+                env_var_assignments += f'{env_var}="{os.environ[env_var]}"\n'
         encoded_assignments = base64.b64encode(
             env_var_assignments.encode("utf-8")
         ).decode("ascii")
@@ -96,9 +102,8 @@ class Cloud(ABC):
         startup_script_lines = self.startup_script.strip().splitlines()
         cd_idx = startup_script_lines.index("cd transient-minecraft")
         new_script_lines = (
-            startup_script_lines[: cd_idx + 1]
-            + [env_file_line]
-            + startup_script_lines[cd_idx + 1 :]
+            startup_script_lines[:cd_idx + 1] + [env_file_line] +
+            startup_script_lines[cd_idx + 1:]
         )
 
         return "".join(f"{line}\n" for line in new_script_lines)
@@ -141,9 +146,9 @@ class GCloud(Cloud):
     def create_instance(self) -> None:
         # Get the latest Debian image
         image_response = (
-            self.compute.images()
-            .getFromFamily(project="debian-cloud", family="debian-9")
-            .execute()
+            self.compute.images().getFromFamily(
+                project="debian-cloud", family="debian-9"
+            ).execute()
         )
         source_disk_image = image_response["selfLink"]
 
@@ -154,50 +159,68 @@ class GCloud(Cloud):
         config = {
             "name": instance_name,
             "machineType": f"zones/{zone}/machineTypes/{machine_type}",
-            "disks": [
+            "disks":
+                [
+                    {
+                        "boot": True,
+                        "autoDelete": True,
+                        "initializeParams": {
+                            "sourceImage": source_disk_image,
+                        },
+                    }
+                ],
+            "networkInterfaces":
+                [
+                    {
+                        "network":
+                            "global/networks/default",
+                        "accessConfigs":
+                            [
+                                {
+                                    "type": "ONE_TO_ONE_NAT",
+                                    "name": "External NAT"
+                                }
+                            ],
+                    }
+                ],
+            "metadata":
                 {
-                    "boot": True,
-                    "autoDelete": True,
-                    "initializeParams": {"sourceImage": source_disk_image,},
-                }
-            ],
-            "networkInterfaces": [
-                {
-                    "network": "global/networks/default",
-                    "accessConfigs": [
-                        {"type": "ONE_TO_ONE_NAT", "name": "External NAT"}
-                    ],
-                }
-            ],
-            "metadata": {
-                "items": [
-                    {"key": "startup-script", "value": self.get_env_startup_script()}
-                ]
+                    "items":
+                        [
+                            {
+                                "key": "startup-script",
+                                "value": self.get_env_startup_script()
+                            }
+                        ]
+                },
+            "serviceAccounts":
+                [
+                    {
+                        "email":
+                            "default",
+                        "scopes":
+                            [
+                                "https://www.googleapis.com/auth/devstorage.read_write",
+                                "https://www.googleapis.com/auth/logging.write",
+                                "https://www.googleapis.com/auth/compute",
+                            ],
+                    }
+                ],
+            "tags": {
+                "items": [os.environ["GCLOUD_FIREWALL_TAG"]]
             },
-            "serviceAccounts": [
-                {
-                    "email": "default",
-                    "scopes": [
-                        "https://www.googleapis.com/auth/devstorage.read_write",
-                        "https://www.googleapis.com/auth/logging.write",
-                        "https://www.googleapis.com/auth/compute",
-                    ],
-                }
-            ],
-            "tags": {"items": [os.environ["GCLOUD_FIREWALL_TAG"]]},
         }
 
         if "GCLOUD_IP" in os.environ:
-            config["networkInterfaces"][0]["accessConfigs"][0]["natIP"] = os.environ[
-                "GCLOUD_IP"
-            ]
+            config["networkInterfaces"
+                  ][0]["accessConfigs"][0]["natIP"] = os.environ["GCLOUD_IP"]
 
         # Create the instance
         project = os.environ["GCLOUD_PROJECT_ID"]
         create_result = (
-            self.compute.instances()
-            .insert(project=project, zone=zone, body=config)
-            .execute()
+            self.compute.instances().insert(
+                project=project, zone=zone, body=config
+            ).execute()
         )
 
         print("Creating instance...")
@@ -205,9 +228,9 @@ class GCloud(Cloud):
         # Poll the operation until it is complete
         while True:
             poll_result = (
-                self.compute.zoneOperations()
-                .get(operation=create_result["id"], project=project, zone=zone)
-                .execute()
+                self.compute.zoneOperations().get(
+                    operation=create_result["id"], project=project, zone=zone
+                ).execute()
             )
 
             if poll_result["status"] == "DONE":
@@ -219,20 +242,23 @@ class GCloud(Cloud):
 
         # Get the IP information from the created instance
         instance_result = (
-            self.compute.instances()
-            .get(instance=poll_result["targetId"], project=project, zone=zone)
-            .execute()
+            self.compute.instances().get(
+                instance=poll_result["targetId"], project=project, zone=zone
+            ).execute()
         )
-        instance_ip = instance_result["networkInterfaces"][0]["accessConfigs"][0][
-            "natIP"
-        ]
+        instance_ip = instance_result["networkInterfaces"][0]["accessConfigs"
+                                                             ][0]["natIP"]
 
-        print(f"Successfully created instance '{instance_name}' on Google Cloud.")
+        print(
+            f"Successfully created instance '{instance_name}' on Google Cloud."
+        )
         print(f"IP: {instance_ip}")
 
     def get_save(self, local_path: str) -> None:
         blobs = list(
-            self.storage_client.list_blobs(bucket_or_name=os.environ["GCLOUD_BUCKET"])
+            self.storage_client.list_blobs(
+                bucket_or_name=os.environ["GCLOUD_BUCKET"]
+            )
         )
         if len(blobs) == 0:
             print("No existing save!")
@@ -267,7 +293,9 @@ class GCloud(Cloud):
         # Get the instance name from the Goole Cloud Metadata server
         instance_name = requests.get(
             "http://metadata.google.internal/computeMetadata/v1/instance/name",
-            headers={"Metadata-Flavor": "Google"},
+            headers={
+                "Metadata-Flavor": "Google"
+            },
         ).text
         print(f"Deleting instance {instance_name} in {zone}")
         self.compute.instances().delete(
@@ -296,12 +324,12 @@ class AWSCloud(Cloud):
         if needs_auth:
             # AWS CLI initial configuration - using AWS CLI for sync command
             aws_commands = [
-                "aws configure set aws_access_key_id %s"
-                % os.environ["AWS_ACCESS_KEY_ID"],
-                "aws configure set aws_secret_access_key %s"
-                % os.environ["AWS_SECRET_ACCESS_KEY"],
-                "aws configure set region %s"
-                % os.environ.get("AWS_REGION", AWSCloud.DEFAULT_REGION),
+                "aws configure set aws_access_key_id %s" %
+                os.environ["AWS_ACCESS_KEY_ID"],
+                "aws configure set aws_secret_access_key %s" %
+                os.environ["AWS_SECRET_ACCESS_KEY"],
+                "aws configure set region %s" %
+                os.environ.get("AWS_REGION", AWSCloud.DEFAULT_REGION),
             ]
 
             for command in aws_commands:
@@ -311,7 +339,9 @@ class AWSCloud(Cloud):
     def required_env_vars(self) -> Sequence[str]:
         env_vars = []
         if self.needs_auth:
-            env_vars += ["AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "AWS_REGION"]
+            env_vars += [
+                "AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "AWS_REGION"
+            ]
         if self.needs_storage:
             env_vars += ["AWS_S3_BUCKET", "AWS_S3_SAVE_KEY"]
         return env_vars
@@ -321,13 +351,14 @@ class AWSCloud(Cloud):
 
         startup_script = self.env_startup_script()
         print(startup_script)
-        with tempfile.TemporaryFile(mode="w", delete=False) as startup_script_file:
+        with tempfile.TemporaryFile(
+            mode="w", delete=False
+        ) as startup_script_file:
             startup_script_file.write(startup_script)
 
         startup_script_uri = (
-            pathlib.PurePath(startup_script_file.name)
-            .as_uri()
-            .replace("file:///", "file://")
+            pathlib.PurePath(startup_script_file.name
+                            ).as_uri().replace("file:///", "file://")
         )
 
         command = (
@@ -381,14 +412,18 @@ class AWSCloud(Cloud):
     def get_save(self, local_path: str) -> None:
         print("Downloading save...")
         subprocess.check_output(
-            shlex.split("aws s3 sync %s %s --delete" % (self._s3_path, local_path))
+            shlex.split(
+                "aws s3 sync %s %s --delete" % (self._s3_path, local_path)
+            )
         )
         print("Download complete.")
 
     def put_save(self, local_path: str) -> None:
         print("Uploading save...")
         subprocess.check_output(
-            shlex.split("aws s3 sync %s %s --delete" % (local_path, self._s3_path))
+            shlex.split(
+                "aws s3 sync %s %s --delete" % (local_path, self._s3_path)
+            )
         )
         print("Upload complete.")
 
