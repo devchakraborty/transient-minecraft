464736a504fe681bffac0025c941d076f65d6584
Dev Chakraborty <devchakraborty@users.noreply.github.com>
2020-04-06 19:48:53 -0700

    Update startup script - pyenv

--- gcloud/startup.sh
+++ gcloud/startup.sh
@@ -5,11 +5,21 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y default-jdk build-essential libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev zlib1g zlib1g-dev git tmux
 
-# Get, build, and install Python 3.7
+# Get pyenv and set up environment
 curl https://pyenv.run | bash
+# Run setup commands both in this session and in future bash sessions
+read -r -d "" pyenv_init_cmd << EOF
+
+# pyenv setup
 export PATH="/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
+
+EOF
+echo "$pyenv_init_cmd" >> ~/.bashrc
+eval "$pyenv_init_cmd"
+
+# Build and install Python 3.7
 pyenv install 3.7.7
 pyenv global 3.7.7
 
